e4edc56eddb47e285eaa8580e9d3ee875709c0aa
ljhkss <l3369671920@outlook.com>
2026-09-17 19:21:16 +0800

    ignore the site dir

--- site/javascripts/homepage.js
+++
@@ -1,8 +0,0 @@
-document$.subscribe(() => {
-  const heroSection = document.querySelector('.hero-section.is-homepage');
-  if (heroSection) {
-    document.body.classList.add('is-homepage');
-  } else {
-    document.body.classList.remove('is-homepage');
-  }
-});
